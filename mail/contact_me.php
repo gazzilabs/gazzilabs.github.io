6ec0dd36c69e220c5330ab1331df53d3a8b97e80
ytcho <ytcho@gazzi.ai>
2021-02-04 16:44:59 +0900

    mail test

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,7 +10,7 @@ if(empty($_POST['name'])  		||
 	return false;
    }
 
-$name = $_POST['name'];
+$name = "=?$charset?B?".base64_encode($_POST['name'])."?=";
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
@@ -19,7 +19,9 @@ $message = $_POST['message'];
 $to = 'ytcho@gazzi.ai'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@gazzi.ai\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "Content-Type: text/html; charset=utf-8\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "From: noreply@gazzi.ai\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
